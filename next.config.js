002d1def0af40dc0bdd8f0b901cbb0e184d40c77
Ondřej Műller <ondmull@gmail.com>
2023-05-31 10:50:50 +0200

    add gpt-ui basePath to next server

--- next.config.js
+++ next.config.js
@@ -1,11 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-  webpack(config) {
-    config.experiments = { ...config.experiments, topLevelAwait: true };
-    return config;
-  },
+    reactStrictMode: true,
+    swcMinify: true,
+    webpack(config) {
+        config.experiments = {...config.experiments, topLevelAwait: true};
+        return config;
+    },
+    basePath: '/gpt-ui',
 };
 
 export default nextConfig;
